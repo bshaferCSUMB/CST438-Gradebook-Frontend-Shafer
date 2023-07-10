5e259af0e1f7c916d324d0a0ab47f9d8d76d71fb
bshaferCSUMB <116942334+bshaferCSUMB@users.noreply.github.com>
2023-07-10 16:12:30 -0700

    Add route to App.js

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ function App() {
        <Switch>
         <Route exact path='/' component={Assignment} />
         <Route path='/gradebook' component={Gradebook} />
+        <Route path='/assignment' component={Assignment} />
        </Switch>
       </BrowserRouter>
     </div>
